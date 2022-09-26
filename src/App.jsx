6eeeca6e99825c345f2917e0e14ac8dd652a7e93
Pappyjay23 <pjinadu02@gmail.com>
2022-09-26 11:42:20 +0100

    Added images to start first 2 sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import Logo from "./assets/images/Digitaly Logo.png";
+import Logo from "./assets/images/Logo.png";
+// import Logo from "./assets/images/Logo.png";
 
 const App = () => {
 	return (
-		<div>
+		<div className="bg-gray-300 h-screen flex items-center justify-center">
 			<img src={Logo} alt='Logo' />
 		</div>
 	);
