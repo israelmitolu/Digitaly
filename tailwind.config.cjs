386cecc913ca437879ded91543a31c8bb34c3c91
Israel Mitolu <israelmitolu@gmail.com>
2022-09-28 06:11:28 +0100

    Add "Awarded works" bg images

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,26 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	theme: {
-		screens: {
-			xxs: "320px",
-			xs: "375px",
-			sm: "425px",
-			md: "768px",
-			lg: "976px",
-			xl: "1024px",
-			xxl: "1440px",
-			sl: "2560px",
-		},
-		extend: {
-			fontFamily: {
-				lato: ["Lato", "sans-serif"],
-				raleway: ["Raleway", "sans-serif"],
-			},
-			backgroundImage: {
-				"t-banner": "url('assets/images/Home Trump Banner.png')",
-			},
-		},
-	},
-	plugins: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      xxs: "320px",
+      xs: "375px",
+      sm: "425px",
+      md: "768px",
+      lg: "976px",
+      xl: "1024px",
+      xxl: "1440px",
+      sl: "2560px",
+    },
+    extend: {
+      fontFamily: {
+        lato: ["Lato", "sans-serif"],
+        raleway: ["Raleway", "sans-serif"],
+      },
+      backgroundImage: {
+        "t-banner": "url('assets/images/Home Trump Banner.png')",
+        "work-banner-1": "url('assets/images/Black waves.jpg')",
+        "work-banner-2": "url('assets/images/Mercury ball.jpg')",
+      },
+    },
+  },
+  plugins: [],
 };
