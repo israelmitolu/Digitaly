51aedb18d246cd35699e6a118a33e4f36d944059
Israel Mitolu <israelmitolu@gmail.com>
2022-09-29 05:03:06 +0100

    Import the correct image

--- src/sections/AwardedWorks/AwardedWorks.jsx
+++ src/sections/AwardedWorks/AwardedWorks.jsx
@@ -1,12 +1,13 @@
-import TrumpTag from "../../assets/images/Trump Tag.png";
 import AwardedWork from "../../components/Work/AwardedWork";
 import ClassicBlueTag from "../../assets/images/Classic Blue Tag.png";
-// import OrangyWaveTag from "../../assets/images/Orangy Wave Tag.png";
+import Crown from "../../assets/images/Awarded Works Crown.png";
+import Tag from "../../components/Tag/Tag";
+import OrangyWaveTag from "../../assets/images/Orangy Wave Tag.png";
 
 const AwardedWorks = () => {
   return (
     <div className="text-[#e5e5e5] pb-[2rem] w-[90%] max-w-[1200px] m-auto px-10 xxl:pt-[3rem]">
-      <div className="flex flex-wrap md:flex-nowrap justify-around items-center mt-[4rem]">
+      <div className="flex justify-between items-center mt-[4rem]">
         <h2 className="uppercase text-[1.5rem] lg:text-[3rem] font-semibold w-full lg:w-[30%] leading-[3rem]">
           Awarded Works
         </h2>
@@ -14,8 +15,7 @@ const AwardedWorks = () => {
           We are passionate digital market, we always produce projects that both
           ourselves and our clients can be truly proud of
         </p>
-        {/* This will be a bigger tag, so its size is subject to change */}
-        <img src={TrumpTag} alt="" className=" w-32" />
+        <Tag imgSrc={Crown} value="290" />
       </div>
 
       {/* Awarded Work 1 */}
@@ -30,7 +30,7 @@ const AwardedWorks = () => {
         labels={["CRO", "Content Marketing", "2021 Awards"]}
         caption="Orangy Wave"
         title="Orangy Wave"
-        icon={ClassicBlueTag}
+        icon={OrangyWaveTag}
         className="bg-work-banner-2"
       />
     </div>
