403442e5f1c4cf4263fe5754ccab342437676b23
Israel Mitolu <israelmitolu@gmail.com>
2022-10-29 12:58:28 +0100

    Add Locomotive scroll

--- src/sections/Deliver/Deliver.jsx
+++ src/sections/Deliver/Deliver.jsx
@@ -6,64 +6,72 @@ import { motion } from "framer-motion";
 import { AnimationContext } from "../../context/animation";
 
 const Deliver = () => {
-	const { riseUpVariant, riseUpItem, tagVariant } =
-		useContext(AnimationContext);
-	return (
-		<div
-			id='about'
-			className='h-full text-white pt-[5rem] pb-[2rem] lg:w-[90%] max-w-[1200px] m-auto px-6 md:px-10 md:pt-[15rem] xxl:pt-[20rem]'>
-			<Stats />
-			<div>
-				<motion.div
-					variants={riseUpVariant}
-					initial='hidden'
-					whileInView='visible'
-					className='flex justify-between items-center mb-8 mt-[4rem]'>
-					<motion.p
-						variants={riseUpItem}
-						className='uppercase text-[1.5rem] font-semibold lg:text-[3rem] w-full lg:w-[30%] leading-[3rem]'>
-						What we deliver
-					</motion.p>
-					<motion.img
-						variants={riseUpItem}
-						className='h-[80px] hidden lg:block'
-						src={Tag1}
-						alt='Tag 1'
-					/>
-					<motion.img
-						variants={tagVariant}
-						className='hidden md:block md:h-[50px] lg:h-[80px]'
-						src={Tag2}
-						alt='Tag 2'
-					/>
-				</motion.div>
-				<motion.div
-					variants={riseUpVariant}
-					initial='hidden'
-					whileInView='visible'
-					className='flex flex-wrap md:flex-nowrap gap-8'>
-					<motion.p
-						variants={riseUpItem}
-						className='text-[#929294] font-medium lg:text-base text-[80%]'>
-						Custom digital strategy informed by a powerful appraisal & your
-						business deserves more than a pre-packed strategy
-					</motion.p>
-					<motion.p
-						variants={riseUpItem}
-						className='text-[#929294] font-medium lg:text-base text-[80%]'>
-						See the progress we are making & what's driving results, anytime, we
-						keep you consistently, transparently informed.
-					</motion.p>
-					<motion.p
-						variants={riseUpItem}
-						className='text-[#929294] font-medium lg:text-base text-[80%]'>
-						Services that dovetail together for your greatest ROI, rowing in the
-						same direction towards your growth goals.
-					</motion.p>
-				</motion.div>
-			</div>
-		</div>
-	);
+  const { riseUpVariant, riseUpItem, tagVariant } =
+    useContext(AnimationContext);
+  return (
+    <div
+      id="about"
+      data-scroll-section
+      className="h-full text-white pt-[5rem] pb-[2rem] lg:w-[90%] max-w-[1200px] m-auto px-6 md:px-10 md:pt-[15rem] xxl:pt-[20rem]"
+    >
+      <Stats />
+      <div>
+        <motion.div
+          variants={riseUpVariant}
+          initial="hidden"
+          whileInView="visible"
+          className="flex justify-between items-center mb-8 mt-[4rem]"
+        >
+          <motion.p
+            variants={riseUpItem}
+            className="uppercase text-[1.5rem] font-semibold lg:text-[3rem] w-full lg:w-[30%] leading-[3rem]"
+          >
+            What we deliver
+          </motion.p>
+          <motion.img
+            variants={riseUpItem}
+            className="h-[80px] hidden lg:block"
+            src={Tag1}
+            alt="Tag 1"
+          />
+          <motion.img
+            variants={tagVariant}
+            className="hidden md:block md:h-[50px] lg:h-[80px]"
+            src={Tag2}
+            alt="Tag 2"
+          />
+        </motion.div>
+        <motion.div
+          variants={riseUpVariant}
+          initial="hidden"
+          whileInView="visible"
+          className="flex flex-wrap md:flex-nowrap gap-8"
+        >
+          <motion.p
+            variants={riseUpItem}
+            className="text-[#929294] font-medium lg:text-base text-[80%]"
+          >
+            Custom digital strategy informed by a powerful appraisal & your
+            business deserves more than a pre-packed strategy
+          </motion.p>
+          <motion.p
+            variants={riseUpItem}
+            className="text-[#929294] font-medium lg:text-base text-[80%]"
+          >
+            See the progress we are making & what's driving results, anytime, we
+            keep you consistently, transparently informed.
+          </motion.p>
+          <motion.p
+            variants={riseUpItem}
+            className="text-[#929294] font-medium lg:text-base text-[80%]"
+          >
+            Services that dovetail together for your greatest ROI, rowing in the
+            same direction towards your growth goals.
+          </motion.p>
+        </motion.div>
+      </div>
+    </div>
+  );
 };
 
 export default Deliver;
--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -6,77 +6,82 @@ import { useContext } from "react";
 import { AnimationContext } from "../../context/animation";
 
 const Footer = () => {
-	const { riseUpVariant2, riseUpItem } = useContext(AnimationContext);
-	return (
-		<>
-			<footer className='pb-[4rem] lg:w-[90%] max-w-[1200px] m-auto px-6 lg:px-10'>
-				<div className='flex flex-col md:flex-row justify-between mt-[4rem]'>
-					{/* Left  */}
-					<motion.div
-						variants={riseUpVariant2}
-						initial='hidden'
-						whileInView='visible'
-						className='md:w-[40%]'>
-						<motion.h2
-							variants={riseUpItem}
-							className='uppercase text-[1.5rem] lg:text-[3rem] font-semibold lg:w-[70%] leading-[3rem] text-[#e5e5e5]'>
-							Keep in Touch
-						</motion.h2>
-						<Link to='home' className='cursor-pointer'>
-							<motion.img
-								variants={riseUpItem}
-								src={Logo}
-								alt='Logo'
-								className='h-[25px] my-4'
-							/>
-						</Link>
-						<motion.p
-							variants={riseUpItem}
-							className='text-[#929294] font-medium mb-8 lg:text-base text-[80%]'>
-							Palau Sebira, Kepulauan Seribu Utara, Kab. Administrasi Kepulauan
-							Seribu, Daerah Khusus Ibukota Jakarta
-						</motion.p>
-						<motion.span variants={riseUpItem}>
-							<FooterLink text='Faq' />
-						</motion.span>
-						<motion.span variants={riseUpItem}>
-							<FooterLink text='Terms & Agreements' />
-						</motion.span>
-					</motion.div>
+  const { riseUpVariant2, riseUpItem } = useContext(AnimationContext);
 
-					{/* Right  */}
-					<motion.div
-						variants={riseUpVariant2}
-						initial='hidden'
-						whileInView='visible'
-						className='md:w-[40%] flex flex-col justify-between'>
-						<motion.span variants={riseUpItem}>
-							<FooterLink text='Email' handle='Hello@Digitaly.yeah' />
-						</motion.span>
-						<motion.span variants={riseUpItem}>
-							<FooterLink text='Instagram' handle='@Digitaly' />
-						</motion.span>
-						<motion.span variants={riseUpItem}>
-							<FooterLink text='Twitter' handle='@Digitaly' />
-						</motion.span>
-						<motion.span variants={riseUpItem}>
-							<FooterLink text='Medium' handle='@Digitaly' />
-						</motion.span>
-						<motion.span variants={riseUpItem}>
-							<FooterLink text='Telegram' handle='@Digitaly' />
-						</motion.span>
-						<motion.span variants={riseUpItem}>
-							<FooterLink text='LinkedIn' handle='@Digitaly' />
-						</motion.span>
-						<motion.span variants={riseUpItem}>
-							<FooterLink text='Facebook' handle='@Digitaly' />
-						</motion.span>
-					</motion.div>
-				</div>
-			</footer>
-			<div className='arch w-[100%] h-4 bg-slate-200 rounded-t-xl' />
-		</>
-	);
+  return (
+    <div data-scroll-section>
+      <footer className="pb-[4rem] lg:w-[90%] max-w-[1200px] m-auto px-6 lg:px-10">
+        <div className="flex flex-col md:flex-row justify-between mt-[4rem]">
+          {/* Left  */}
+          <motion.div
+            variants={riseUpVariant2}
+            initial="hidden"
+            whileInView="visible"
+            className="md:w-[40%]"
+          >
+            <motion.h2
+              variants={riseUpItem}
+              className="uppercase text-[1.5rem] lg:text-[3rem] font-semibold lg:w-[70%] leading-[3rem] text-[#e5e5e5]"
+            >
+              Keep in Touch
+            </motion.h2>
+            <Link to="home" className="cursor-pointer">
+              <motion.img
+                variants={riseUpItem}
+                src={Logo}
+                alt="Logo"
+                className="h-[25px] my-4"
+              />
+            </Link>
+            <motion.p
+              variants={riseUpItem}
+              className="text-[#929294] font-medium mb-8 lg:text-base text-[80%]"
+            >
+              Palau Sebira, Kepulauan Seribu Utara, Kab. Administrasi Kepulauan
+              Seribu, Daerah Khusus Ibukota Jakarta
+            </motion.p>
+            <motion.span variants={riseUpItem}>
+              <FooterLink text="Faq" />
+            </motion.span>
+            <motion.span variants={riseUpItem}>
+              <FooterLink text="Terms & Agreements" />
+            </motion.span>
+          </motion.div>
+
+          {/* Right  */}
+          <motion.div
+            variants={riseUpVariant2}
+            initial="hidden"
+            whileInView="visible"
+            className="md:w-[40%] flex flex-col justify-between"
+          >
+            <motion.span variants={riseUpItem}>
+              <FooterLink text="Email" handle="Hello@Digitaly.yeah" />
+            </motion.span>
+            <motion.span variants={riseUpItem}>
+              <FooterLink text="Instagram" handle="@Digitaly" />
+            </motion.span>
+            <motion.span variants={riseUpItem}>
+              <FooterLink text="Twitter" handle="@Digitaly" />
+            </motion.span>
+            <motion.span variants={riseUpItem}>
+              <FooterLink text="Medium" handle="@Digitaly" />
+            </motion.span>
+            <motion.span variants={riseUpItem}>
+              <FooterLink text="Telegram" handle="@Digitaly" />
+            </motion.span>
+            <motion.span variants={riseUpItem}>
+              <FooterLink text="LinkedIn" handle="@Digitaly" />
+            </motion.span>
+            <motion.span variants={riseUpItem}>
+              <FooterLink text="Facebook" handle="@Digitaly" />
+            </motion.span>
+          </motion.div>
+        </div>
+      </footer>
+      <div className="arch w-[100%] h-4 bg-slate-200 rounded-t-xl" />
+    </div>
+  );
 };
 
 export default Footer;
